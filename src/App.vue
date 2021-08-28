c6519d29a20d895f3fecf3eefb14a53818f530b0
manisha2307 <vallerimanisha2307@gmail.com>
2021-08-29 01:05:54 +0530

    first

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="App">
+    <ImageGrid />
+  </div>
+</template>
+
+<script>
+import ImageGrid from './components/ImageGrid.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ImageGrid
+  }
+}
+</script>
